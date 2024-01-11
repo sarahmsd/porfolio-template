97c2a1180380fea06dd8532416e4e4a6461ca097
Saratou Diallo <sarahdiallo98@gmail.com>
2024-01-11 23:37:18 +0000

    Initialization

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'text': '#3B3B3B',
+        'pink': '#FF7999',
+      },
+    },
+  },
+  plugins: [],
+}
+
